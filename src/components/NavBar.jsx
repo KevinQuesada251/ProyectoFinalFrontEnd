a684e377b815cbef0b287704278207b8612af2c1
KevinQuesada251 <kquesada988@gmail.com>
2025-03-11 23:38:27 -0600

    primer commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>Inicio</li>
+                <li>Foro</li>
+                <li>juegos</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
